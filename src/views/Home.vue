8afce6eed8b125db1c657284bf4c61033b5a4688
Let_J <devilc0822@gmail.com>
2022-02-18 13:44:48 +0800

    fix bugs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,67 +1,64 @@
 <template>
-	<el-container class="home-container">
-		<el-header>
+  <el-container class="home-container">
+    <el-header>
       <h2>
-      电商后台管理系统
+        电商后台管理系统
       </h2>
     </el-header>
-		<el-container>
-			<el-aside width='initial'>
-				<Aside></Aside>
-			</el-aside>
-			<el-container>
-				<el-main>
-					<Main>
+    <el-container>
+      <el-aside width='initial'>
+        <Aside></Aside>
+      </el-aside>
+      <el-container>
+        <el-main>
+          <Main>
             <router-view></router-view>
           </Main>
-				</el-main>
-			</el-container>
-		</el-container>
-	</el-container>
+        </el-main>
+      </el-container>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import Aside from '@/views/layout/Aside.vue'
-import Main from '@/views/layout/Main.vue'
+  // @ is an alias to /src
+  import Aside from '@/views/layout/Aside.vue'
+  import Main from '@/views/layout/Main.vue'
 
-export default {
-	name: 'Home',
-	components: {
-		Aside,
-		Main,
-	},
-	data() {
-		const item = {
-			date: '2016-05-02',
-			name: '王小虎',
-			address: '上海市普陀区金沙江路 1518 弄',
-		}
-		return {
-			tableData: Array(20).fill(item),
-		}
-	},
-}
+  export default {
+    name: 'Home',
+    components: {
+      Aside,
+      Main,
+    },
+    data() {
+      return {
+
+      }
+    },
+  }
 </script>
 
 <style lang="scss" scoped>
-.home-container {
-	height: 100vh;
-	// min-width: 1024px;
-}
-.el-header {
-	background-color: #b3c0d1;
-	color: #333;
-	line-height: 60px;
-}
-.el-main {
-	padding: 0;
-  background-color: #f5f5f5;
-}
+  .home-container {
+    height: 100vh;
+    // min-width: 1024px;
+  }
 
-.el-aside {
-	background-color: #334054;
-	color: #bfcdb9;
-	// width: initial !important;
-}
+  .el-header {
+    background-color: #b3c0d1;
+    color: #333;
+    line-height: 60px;
+  }
+
+  .el-main {
+    padding: 0;
+    background-color: #f5f5f5;
+  }
+
+  .el-aside {
+    background-color: #334054;
+    color: #bfcdb9;
+    // width: initial !important;
+  }
 </style>
--- src/views/layout/Main.vue
+++ src/views/layout/Main.vue
@@ -1,97 +1,104 @@
 <template>
-	<div class="main">
-		<div class="main-header">
-			<div>
-				<!-- <i v-show="isFold" @click="changeFold" class="el-icon-s-fold"></i>
+  <div class="main">
+    <div class="main-header">
+      <div>
+        <!-- <i v-show="isFold" @click="changeFold" class="el-icon-s-fold"></i>
 				<i v-show="!isFold" @click="changeFold" class="el-icon-s-unfold"></i> -->
-			</div>
-			<el-breadcrumb separator-class="el-icon-arrow-right" ref='breadcrumb' v-show='isShowHomeBreadcrumb'>
-				<el-breadcrumb-item @click.native='goHome'>首页</el-breadcrumb-item>
-				<el-breadcrumb-item v-for="(item,index) in breadcrumb" :key='index'>{{item}}</el-breadcrumb-item>
-			</el-breadcrumb>
-		</div>
-		<router-view></router-view>
-	</div>
+      </div>
+      <el-breadcrumb separator-class="el-icon-arrow-right" ref='breadcrumb' v-show='isShowHomeBreadcrumb'>
+        <el-breadcrumb-item @click.native='goHome'>首页</el-breadcrumb-item>
+        <el-breadcrumb-item v-for="(item,index) in breadcrumb" :key='index'>{{item}}</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-export default {
-	name: 'Main',
-	data() {
-		return {
-			isFold: true,
-			breadcrumb: [],
-      isShowHomeBreadcrumb:true
-		}
-	},
-	methods: {
-		changeFold() {
-			this.isFold = this.isFold ? false : true
-			this.$bus.$emit('changeFold', this.isFold)
-		},
-		find(str, arr) {
-			for (const item of arr) {
-        item.children
-				if (item.children) {
-					for (const itemChild of item.children) {
-						if (itemChild.path == str) return {child:itemChild,parent:item}
-					}
-				}
-			}
-		},
-    goHome() {
-      this.$router.push('/home')
-      this.breadcrumb.length = 0
-      window.sessionStorage.setItem('breadcrumb',this.breadcrumb)
-    }
-	},
-  created() {
-    this.breadcrumb = window.sessionStorage.getItem('breadcrumb').split(',')
-  },
-	mounted() {
-		this.$bus.$on('sendBreadcrumb', (key, asideNavInfo) => {
-			let breadcrumb = []
-			breadcrumb = key.trim().split('/')
-			if (!breadcrumb[0]) {
-				breadcrumb.shift()
-			}
-			let needCheck = breadcrumb.pop()
-      let res = this.find(needCheck,asideNavInfo)
-      this.breadcrumb.length = 0
-      this.breadcrumb.push(res.parent.authName,res.child.authName)
-      window.sessionStorage.setItem('breadcrumb',this.breadcrumb)
-		})
-	},
-}
+  export default {
+    name: 'Main',
+    data() {
+      return {
+        isFold: true,
+        breadcrumb: [],
+        isShowHomeBreadcrumb: true
+      }
+    },
+    methods: {
+      changeFold() {
+        this.isFold = this.isFold ? false : true
+        this.$bus.$emit('changeFold', this.isFold)
+      },
+      find(str, arr) {
+        for (const item of arr) {
+          item.children
+          if (item.children) {
+            for (const itemChild of item.children) {
+              if (itemChild.path == str) return {
+                child: itemChild,
+                parent: item
+              }
+            }
+          }
+        }
+      },
+      goHome() {
+        this.$router.push('/home')
+        this.breadcrumb.length = 0
+        window.sessionStorage.setItem('breadcrumb', this.breadcrumb)
+      }
+    },
+    created() {
+      if (window.sessionStorage.getItem('breadcrumb')) {
+        this.breadcrumb = window.sessionStorage.getItem('breadcrumb').split(',')
+      }
+    },
+    mounted() {
+      this.$bus.$on('sendBreadcrumb', (key, asideNavInfo) => {
+        let breadcrumb = []
+        breadcrumb = key.trim().split('/')
+        if (!breadcrumb[0]) {
+          breadcrumb.shift()
+        }
+        let needCheck = breadcrumb.pop()
+        let res = this.find(needCheck, asideNavInfo)
+        this.breadcrumb.length = 0
+        this.breadcrumb.push(res.parent.authName, res.child.authName)
+        window.sessionStorage.setItem('breadcrumb', this.breadcrumb)
+      })
+    },
+  }
 </script>
 
 <style lang="scss" scoped>
-.main {
-  height:100%;
-  overflow:hidden;
+  .main {
+    height: 100%;
+    overflow: hidden;
 
-	.main-header {
-		display: flex;
-		align-items: center;
-        padding: 15px 0 0 15px;
-		i {
-			font-size: 36px;
-			margin-right: 18px;
-			cursor: pointer;
-			padding: 8px;
-			&:hover {
-				background-color: rgba(0, 0, 0, 0.025);
-			}
-		}
+    .main-header {
+      display: flex;
+      align-items: center;
+      padding: 15px 0 0 15px;
 
-    .el-breadcrumb{
-      font-size: 16px;
+      i {
+        font-size: 36px;
+        margin-right: 18px;
+        cursor: pointer;
+        padding: 8px;
+
+        &:hover {
+          background-color: rgba(0, 0, 0, 0.025);
+        }
+      }
+
+      .el-breadcrumb {
+        font-size: 16px;
+      }
+
+      .el-breadcrumb .el-breadcrumb__item:first-child {
+        cursor: pointer;
+        font-weight: bold;
+      }
     }
-
-    .el-breadcrumb .el-breadcrumb__item:first-child{
-      cursor: pointer;
-      font-weight: bold;
-    }
-	}
-}
+  }
 </style>
