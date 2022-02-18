c7159f93dda05f75bee225a2881bed6488df4bd6
Let_J <devilc0822@gmail.com>
2022-02-18 13:49:02 +0800

    reports

--- src/views/mainPage/Reports.vue
+++ src/views/mainPage/Reports.vue
@@ -1,15 +1,41 @@
 <template>
   <div class='Reports-view'>
-    数据图表
+    <el-card class="box-card">
+
+    </el-card>
   </div>
 </template>
 
 <script>
-export default {
+  export default {
+    name: 'Reports',
+    data() {
+      return {
 
-}
+      }
+    },
+    created() {
+      this.getReports()
+    },
+    methods: {
+      async getReports() {
+        const {
+          data: res
+        } = await this.$http({
+          url: 'reports/type/1',
+          method: 'get'
+        })
+        console.log(res)
+      }
+
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
-
+  .Reports-view {
+    .box-card {
+      margin: 15px;
+    }
+  }
 </style>
